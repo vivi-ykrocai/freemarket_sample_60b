32dcd094c9451f97a7f37cfffad8609f1e4880cd
akane.simauma <daphne.78216simaoki@gmail.com>
2019-10-09 14:57:37 +0900

    user-session  preparation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   'routes#index'
+  resources :signup do
+    collection do
+      get 'step1'
+      get 'step2'
+      get 'step3'
+      get 'step4' 
+      get 'finish' 
+    end
+  end
 end
+
