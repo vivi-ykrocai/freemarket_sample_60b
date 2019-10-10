14b1d5e07aedb9c27a7ecdead4426ddd5f9e5e98
kudotaka0421 <zmk1n421@gmail.com>
2019-10-10 21:45:22 +0900

    部分テンプレの呼び出し、編集ページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def edit
+  end
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :items
-  root 'items#index'
+  # マージするときに、root 'items#index'に戻す。
+  root 'users#edit'
   resources :users, only: [:index, :edit, :update]
   resources :cards, only: [:index, :edit, :update]
   resources :logouts, only: [:index]
