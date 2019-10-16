c253637bb971a374f07e8b9e6f73dfb05f25178b
sato-kai <h175bkznpicloud.com>
2019-10-16 17:05:40 +0900

    binding.pry削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,7 +14,6 @@ class ItemsController < ApplicationController
   end
 
   def create
-    # binding.pry
     Item.create(name: item_params[:name], image: item_params[:image], item_status: item_params[:item_status], delivery_charged: item_params[:delivery_charged], delivery_method: item_params[:delivery_method], delivery_area: item_params[:delivery_area], delivery_shipping_date: item_params[:delivery_shipping_date], total_price: item_params[:total_price], item_profile_comment: item_params[:item_profile_comment], item_salse_status: item_params[:item_salse_status], good: item_params[:good])
     redirect_to action: :index
   end
