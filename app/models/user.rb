4fb55227b6c6f68e055cffa2fa50949293c4d8a2
akane.simauma <daphne.78216simaoki@gmail.com>
2019-10-17 19:00:32 +0900

    sns認証の記述追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,53 @@ class User < ApplicationRecord
   has_many :saling_items, -> { where("buyer_id is NULL") }, foreign_key: "seller_id", class_name: "Item"
   has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "seller_id", class_name: "Item"
   has_many :sns_credentials, dependent: :destroy
+
+
+  def self.without_sns_data(auth)
+    user = User.where(email: auth.info.email).first
+
+      if user.present?
+        sns = SnsCredential.creae(
+          uid: auth.uid,
+          provider: auth.provider,
+          user_id: user.id
+        )
+      else
+        user = User.new(
+          nickname: auth.info.name,
+          email:auth.info.email,
+        )
+        sns = SnsCredential.new(
+          uid:auth.uid,
+          provider: auth.provider
+        )
+      end
+      return  { user: user, sns: sns }
+    end
+
+    def self.with_sns_data(auth, snscredential)
+      user = User.where(id: snscredential.user_id).first
+      unless user.present?
+        user = User.new(
+          nickname: auth.info.name,
+          email: auth.info.email,
+        )
+      end
+      return {user: user}
+    end
+
+    def self.find_oauth(auth)
+      uid = auth.uid
+      provider = auth.provider
+      snscredential = SnsCredential.where(uid: uid, provider: provider).first
+      if snscredential.present?
+        user = with_sns_data(auth, snscredential)[:user]
+        sns = SnsCredential
+      else
+        user = without_sns_data(auth)[:user]
+        sns = without_sns_data(auth)[:sns]
+      end
+      return { user: user ,sns: sns}
+    end
 end
-
-
-
 
