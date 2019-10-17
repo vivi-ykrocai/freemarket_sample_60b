9a523fd3f3578d8646cb9d052d36fa6d1f24156e
kudotaka0421 <zmk1n421@gmail.com>
2019-10-17 14:00:26 +0900

    パンくずのhaml,css作成　→　部分テンプレに保存

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,53 +1,56 @@
+# 一旦全てroot_pathで指定する。
+# あとで必要があれば個別のリンク先を指定する。工藤 10月17日(木)
+
 crumb :root do
   link "メルカリ", root_path
 end
 
 crumb :mypage do
-  link 'マイページ', user_path(current_user.id)
+  link 'マイページ', root_path
   parent :root
 end
 
 crumb :mypage_logout do
-  link 'ログアウト', logouts_path
+  link 'ログアウト', root_path
   parent :mypage
 end
 
 crumb :mypage_listing do
-  link '出品した商品-出品中', listings_path
+  link '出品した商品-出品中', root_path
   parent :mypage
 end
 
 crumb :mypage_progression do
-  link '出品した商品-取引中', progressions_path
+  link '出品した商品-取引中', root_path
   parent :mypage
 end
 
 crumb :mypage_completion do
-  link '出品した商品-売却済', completions_path
+  link '出品した商品-売却済', root_path
   parent :mypage
 end
 
 crumb :mypage_purchase do
-  link '購入した商品-取引中', purchases_path
+  link '購入した商品-取引中', root_path
   parent :mypage
 end
 
 crumb :mypage_purchased do
-  link '出品した商品-過去の取引', purchased_index_path
+  link '出品した商品-過去の取引', proot_path
   parent :mypage
 end
 
 crumb :mypage_profile do
-  link 'プロフィール', user_profile_path
+  link 'プロフィール', root_path
   parent :mypage
 end
 
 crumb :mypage_credit_card do
-  link '支払い方法', new_credit_card_path
+  link '支払い方法', root_path
   parent :mypage
 end
 
 crumb :mypage_identification do
-  link '本人情報', identifications_path
+  link '本人情報', root_path
   parent :mypage
 end
