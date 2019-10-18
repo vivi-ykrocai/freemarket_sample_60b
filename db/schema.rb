62881875a6c266903e1f3f69bc512e65bbdd622b
Yukari Ochiai <ykr.ocai.vivid@gmail.com>
2019-10-18 09:52:19 +0900

    imageモデル及びテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_17_050134) do
+ActiveRecord::Schema.define(version: 2019_10_18_004837) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id", null: false
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 2019_10_17_050134) do
     t.string "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_images_on_item_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -84,5 +92,6 @@ ActiveRecord::Schema.define(version: 2019_10_17_050134) do
   end
 
   add_foreign_key "addresses", "users"
+  add_foreign_key "images", "items"
   add_foreign_key "items", "categories"
 end
