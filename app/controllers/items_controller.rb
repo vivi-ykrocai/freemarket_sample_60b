027319e985b3b4a83ab6ad0f60c5042ae0010dd0
kudotaka0421 <zmk1n421@gmail.com>
2019-10-18 13:39:27 +0900

    items#edit　のコントローラ設定、haml、cssファイルの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,11 +15,16 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+  end
+
+
+
   def create
     Item.create(name: item_params[:name], image: item_params[:image], item_status: item_params[:item_status], delivery_charged: item_params[:delivery_charged], delivery_method: item_params[:delivery_method], delivery_area: item_params[:delivery_area], delivery_shipping_date: item_params[:delivery_shipping_date], total_price: item_params[:total_price], item_profile_comment: item_params[:item_profile_comment], item_salse_status: item_params[:item_salse_status], good: item_params[:good], category_id: item_params[:category_id])
     redirect_to action: :index
   end
-  
+
   def pay
     # @item = Item.find(params[:id])
     Payjp.api_key = ENV['PAYJP_ACCESS_KEY']
