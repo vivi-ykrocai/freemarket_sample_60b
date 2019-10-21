0322fdaee6217cbf81149a2db42d526374e4c6ba
Tobita0116 <kagashow0116@gmail.com>
2019-10-21 14:21:51 +0900

    DB:cards_table作成
    カード情報DBを作成
    readmeにcards_table記述
    addresses_table削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_19_103224) do
+ActiveRecord::Schema.define(version: 2019_10_21_043205) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id", null: false
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2019_10_19_103224) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -66,30 +75,24 @@ ActiveRecord::Schema.define(version: 2019_10_19_103224) do
     t.string "family_name_kana", null: false
     t.string "last_name_kana", null: false
     t.date "birthday", null: false
-    t.string "iine"
-    t.string "mer_point"
-    t.string "icon_image"
-    t.text "profiile_comments"
-    t.integer "number_of_exhibits"
-    t.integer "number_of_evaluations"
-    t.string "excerent"
-    t.string "good"
-    t.string "bad"
+    t.string "phone_number"
     t.string "postal_code"
     t.string "prefectures"
     t.string "city"
     t.string "address"
     t.string "building_name"
+    t.string "phone_number2"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "phone_number"
-    t.string "phone_number2"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "addresses", "users"
+  add_foreign_key "cards", "users"
   add_foreign_key "images", "items"
   add_foreign_key "items", "categories"
 end
