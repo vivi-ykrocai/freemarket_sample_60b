be8f2b9a07ec3a06de2723656850e1454f298d8e
akane.simauma <daphne.78216simaoki@gmail.com>
2019-10-22 19:29:48 +0900

    環境変数の設定変更

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -262,11 +262,11 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-
+if Rails.env.development?
   config.omniauth :facebook, ENV['FACEBOOK_CLIENT_ID'], ENV['FACEBOOK_CLIENT_SECRET']
   # , strategy_class: OmniAuth::Strategies::Facebook
   config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], skip_jwt: true
-
+end
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
