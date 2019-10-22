424bebfb2c6bc86e72b7b64a6d98fdda5f0110ef
akane.simauma <daphne.78216simaoki@gmail.com>
2019-10-22 20:04:22 +0900

    コメントアウト戻し

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  # omniauth
-  # controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
-  #   registrations: 'users/registrations' }
+  devise_for :users,
+  controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations' }
  
   root 'items#index'
 
