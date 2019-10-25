82b916a9703cbb5fb8d0f5526cd7221b5f29a153
sato-kai <h175bkznpicloud.com>
2019-10-25 19:38:01 +0900

    update時にカラムの追加を許可する
    バリデーションは新規作成(create時のみにする)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
 
   private
 
@@ -16,5 +18,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[nick_name last_name family_name last_name_kana family_name_kana birthday phone_number postal_code prefectures city address building_name phone_number2 iine mer_point icon_image phone_number2])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nick_name, :icon_image, :profiile_comments])
   end
+
 end
