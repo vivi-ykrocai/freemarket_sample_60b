db1ac00214ee91735af39e689904c6cd3b2b47cf
sato-kai <h175bkznpicloud.com>
2019-10-30 12:10:14 +0900

    商品が編集・更新できるかのテスト

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,42 +1,57 @@
 require 'rails_helper'
 
 describe ItemsController do
-  describe 'DELETE destroy' do
-  let(:user) { build(:user) }
-    context 'can delete' do
-      # ログイン中ユーザー＝出品者ユーザーの場合、アイテムを削除できる
-      it '@item.seller_id == current_user.id' do
-        login user
-        item = create(:item, seller_id: 10)
-        expect do
-          delete :destroy, params: { id: item.id }, session: {}
-        end.to change(Item, :count).by(-1)
-      end
-      # 削除に成功した場合root_pathに移動する
-      it 'redirects to root_path' do
-        login user
-        item = create(:item, seller_id:10)
-        delete :destroy, params: { id: item.id }, session: {}
-        expect(response).to redirect_to root_path(user)
-      end
+
+  # 商品編集機能
+  describe 'GET #edit' do
+
+    it 'has a 200 status code' do
+      get :edit, params: { id: 1 }
+      expect(response).to have_http_status(200)
     end
-    context 'can not delete' do
-      # ログイン中ユーザー！＝出品者ユーザーの場合、アイテムを削除できない
-      it '@item.seller_id != current_user.id' do
+    it "assigns the requested item to @item" do
+      item = create(:item)
+      get :edit, params: {id: item.id}
+      expect(assigns(:item)).to eq item
+    end
+    it "renders the :edit template" do
+      item = create(:item)
+      get :edit, params: {id: item.id}
+      expect(response).to render_template :edit
+    end
+
+  end
+
+  # 商品更新
+  describe 'PATCH #update' do
+  let(:user) { create(:user) }
+    context 'can update' do
+      it 'item can update' do
+        item = create(:item)
+        update_attributes = {delivery_area: '北海道'}
         login user
-        item = create(:item, seller_id: 20)
-        expect do
-          delete :destroy, params: { id: item.id }, session: {}
-        end.to change(Item, :count).by(0)
+        patch :update, params: {id: item.id, item: update_attributes}
+        expect(item.reload.delivery_area).not_to eq '東京都'
       end
-      # ログイン中ユーザー！＝出品者ユーザーの場合、アイテムを削除できず、detail_item_pathに飛ぶ
-      it "redirect to detail_item_path" do
+      it 'redirect_to detail_item_path(@item)' do
+        item = create(:item)
+        update_attributes = {delivery_area: '北海道'}
         login user
-        item = create(:item, seller_id:20)
-        delete :destroy, params: { id: item.id }, session: {}
+        patch :update, params: {id: item.id, item: update_attributes}
         expect(response).to redirect_to detail_item_path(item)
       end
     end
+    context 'can not update' do
+      it 'can not update' do
+        item = create(:item)
+        update_attributes = {delivery_area: ''}
+        login user
+        patch :update, params: {id: item.id, item: update_attributes}
+        expect(item.reload.delivery_area).to eq '東京都'
+      end
+    end
   end
+
+
 end
 
